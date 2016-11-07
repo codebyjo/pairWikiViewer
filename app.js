19a2f4e4a360a2a7d12bcd440db02c71442898ba
Eduardo Garcia <eagarcia8@gmail.com>
2016-11-06 19:59:29 -0800

    Attempts at API calls

--- app.js
+++ app.js
@@ -1,13 +1,37 @@
-	//search and see the resulting Wikipedia entries. -->
-	//click a button to see a random Wikipedia entry. -->
+//search and see the resulting Wikipedia entries. -->
 
-	  var luckySearchBtn = document.getElementById('luckySearch');
+var function searchArticle(query){
+	//clear current content
 
-	  //add event listener
-	  luckySearchBtn.addEventListener('click', function(event) {
-	    window.location.href='https://en.wikipedia.org/wiki/Special:Random';
-	  });
+	//allow search button to search Wikipedia for article
 
+	var url = "https://en.wikipedia.org/w/api.php?format=json&action=parse&section=0&prop=text&page=" + query;
 
-	//type in the search box, I can see a dropdown menu with autocomplete options for matching Wikipedia entries-->
-    
+	$.get(url, function(results) {
+
+		console.log(results)
+
+	});
+	//pull title and first paragraph of search object
+
+	//allow for certain number of searches to display
+}
+
+var normalSearchBtn = document.getElementById('normalSearch');
+
+normalSearchBtn.addEventListener('click', function(event) {
+
+	searchArticle( $('#normalSearch').value )
+
+	});
+
+//click a button to see a random Wikipedia entry. -->
+
+var luckySearchBtn = document.getElementById('luckySearch');
+
+	//add event listener
+luckySearchBtn.addEventListener('click', function(event) {
+	window.location.href='https://en.wikipedia.org/wiki/Special:Random';
+});
+
+//type in the search box, I can see a dropdown menu with autocomplete options for matching Wikipedia entries-->
