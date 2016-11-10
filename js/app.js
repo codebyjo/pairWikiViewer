f173410a06f8c546b44689d12509bb5327da3c3d
Eduardo Garcia <eagarcia8@gmail.com>
2016-11-10 14:59:25 -0800

    Corrected Regex comparator, CSS for search buttons refined.

--- js/app.js
+++ js/app.js
@@ -1,24 +1,42 @@
 $("document").ready(function() {
   
+  valid = new RegExp(/[a-zA-Z0-9]/g);
+  resultsArray = [];
+  
+  $(".button").hover(function() {
+    $(this).css({
+      "color": "#000000",
+      "border": "1px solid rgb(198, 198, 198)",
+      "box-shadow": "0 1px rgb(240, 240, 240)"
+    });
+  }, function() {
+    $(this).css({
+      "color": "rgb(117, 117, 117)",
+      "border": "none",
+      "box-shadow": "none"
+    });
+  });
+  
   // Search Wikipedia.
   function searchArticle(searchQuery) {
-	//Clear current content
-  
-  //Makes sure that the search box has at least 1 character or 1 number, otherwise open a random page.
-  if (searchQuery == "" || searchQuery.test(/[a-zA-Z0-9]/) ) {
-    window.open("https://en.wikipedia.org/wiki/Special:Random", '_blank');
-  };
+    //Clear current content
     
-  // Request list of possible articles.
-	var searchUrl = "https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=" + searchQuery + "&utf8=";
+    //Makes sure that the search box has at least 1 character or 1 number, otherwise open a random page.
+    if (valid.test(searchQuery) == false) {
+      $("#searchBox").attr("placeholder", "Type something in you noob!");
+    }else{
+    
+	    var searchUrl = "https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=" + searchQuery + "&utf8=";
 
-	$.ajax({
-    url: searchUrl,
-    dataType: "jsonp",
-    success: function(results) {
-		console.log(results);
-	  }
-  });
+      // Request list of possible articles.
+      $.ajax({
+        url: searchUrl,
+        dataType: "jsonp",
+        success: function(results) {
+          resultsArray = results.query.search;
+	      }
+      });
+    }
     
 	//pull title and first paragraph of search object
 
@@ -29,7 +47,6 @@ $("document").ready(function() {
   
   // Normal Search button function
   $("#normalSearch").on("click", function(event) {
-    console.log($("#searchBox").val());
     searchArticle($("#searchBox").val());
 	});
   
