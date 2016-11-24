bfda84dff0c4d2405f3fb7f9cb0717c556a3adde
Joanne D <codebyjo@gmail.com>
2016-11-24 00:10:11 -0500

    populate titles

--- js/app.js
+++ js/app.js
@@ -2,6 +2,14 @@ $("document").ready(function() {
 
   valid = new RegExp(/[a-zA-Z0-9]/g);
   resultsArray = [];
+  var resultsHTML = function(arrayInput){
+    var htmlOutput = '';
+    for (var i=0; i<arrayInput.length; i++){
+      htmlOutput += '<div>' + arrayInput[i].title + '</div>';
+    }
+    return htmlOutput;
+
+  };
   secondPageHeader = '\
     <div style="background-color: rgb(241, 241, 241); width: 100%; height: 60px; margin: 0; padding: 0;">\
       <img src="images/Wikipedia-logo-wordmark.png" alt="Wikipedia Logo" style="height: 58px;" />\
@@ -9,14 +17,12 @@ $("document").ready(function() {
       <button id="normalSearch" class="loaded-search" type="submit" style="background-color: rgb( 66, 133, 244); position: relative; top: -23px; left: -5px; height: 41px; width: 41px; font-family: \'Times New Roman\', serif; font-size: 1.25em; color: white; border: none;"><i class="fa fa-search" aria-hidden="true"></i></button>\
     </div>\
     <div style="border-bottom: 1px solid rgb(235, 235, 235); height: 60px; padding-left: 200px;">\
-		  <ul>\
-		    <li style="width: 30px; font-size: 13px; font-weight: 600; height: 58px; text-align: center; border-bottom: 3px solid rgb(66, 133, 244); color: rgb(66, 133, 244); line-height: 60px; vertical-align: middle;">All</li>\
-		  </ul>\
-		</div>\
+      <ul>\
+        <li style="width: 30px; font-size: 13px; font-weight: 600; height: 58px; text-align: center; border-bottom: 3px solid rgb(66, 133, 244); color: rgb(66, 133, 244); line-height: 60px; vertical-align: middle;">All</li>\
+      </ul>\
+    </div>\
     ';
-  secondPageArticle = '\
-  <div>snippet</div>\
-  ';
+  secondPageArticle = '<div>snippet</div>';
 
   $(".button").hover(function() {
     $(this).css({
@@ -38,29 +44,30 @@ $("document").ready(function() {
     // Normal Search button function
     $("#normalSearch").on("click", function(event) {
       searchArticle($("#searchBox").val());
-	  });
+    });
 
     //Makes sure that the search box has at least 1 character or 1 number, otherwise open a random page.
     if (valid.test(searchQuery) == false) {
       $("#searchBox").attr("placeholder", "Type something in you noob!");
     }else{
 
-	    var searchUrl = "https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=" + searchQuery + "&utf8=";
+      var searchUrl = "https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=" + searchQuery + "&utf8=";
 
+      $("header").empty();
+      $("article").empty();
       // Request list of possible articles.
       $.ajax({
         url: searchUrl,
         dataType: "jsonp",
         success: function(results) {
           resultsArray = results.query.search;
-          console.log(results.query.search);
-	      }
+          //console.log(results.query.search);
+          $("header").html(secondPageHeader);
+          console.log(resultsArray);
+          secondPageArticle = resultsHTML(resultsArray);
+          $("article").html(secondPageArticle); // HTML results go here (referenced by variable)
+        }
       });
-
-      $("header").empty();
-      $("article").empty();
-      $("header").html(secondPageHeader);
-      $("article").html(); // HTML results go here (referenced by variable)
 
       // Disconnected listener, so have to relisten.
       $("#searchBox").keypress(function (e) {
@@ -76,12 +83,12 @@ $("document").ready(function() {
       $("#searchBox").attr("value", searchQuery);
     }
 
-	//pull title and first paragraph of search object
+  //pull title and first paragraph of search object
   //var title = callback.query.search[m].title;
   //var url = title.replace(/ /g, " ");
 
   //$ (".title")
-	//allow for certain number of searches to display
+  //allow for certain number of searches to display
   }
 
   //type in the search box, I can see a dropdown menu with autocomplete options for matching Wikipedia entries-->
@@ -118,7 +125,7 @@ $("#searchBox").autocomplete({
   // Normal Search button function
   $("#normalSearch").on("click", function(event) {
     searchArticle($("#searchBox").val());
-	});
+  });
 
   // Lucky Search button function
   $("#luckySearch").on("click", function(event) {
