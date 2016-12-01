914451765a740e6dad10eedcc9d3ac232fad8ec6
Eduardo Garcia <eagarcia8@gmail.com>
2016-11-30 18:53:17 -0800

    Fixed search term carry over, added styling for results, fixed listeners for 'second' page.

--- js/app.js
+++ js/app.js
@@ -1,19 +1,35 @@
 $("document").ready(function() {
 
   valid = new RegExp(/[a-zA-Z0-9]/g);
+  
   resultsArray = [];
-  var resultsHTML = function(arrayInput){
-    var htmlOutput = '';
-    for (var i=0; i<arrayInput.length; i++){
-      htmlOutput += '<div style="width: 80%; margin: 0 auto; background-color: white;"><div style="width: 100%; padding: 10px; font-size: 13px;"><a href="https://en.wikipedia.org/wiki/' + arrayInput[i].title + '"target="_blank"><h3>' + arrayInput[i].title + '</h3><p>' + arrayInput[i].snippet + '</p></a></div></div>';
+  
+  var resultsHTML = function(arrayInput) {
+    
+    var htmlOutput = '\
+    <p style="font-size: 13px; padding-left: 200px; margin-top: -20px; margin-bottom: 20px; color: #808080;">About ' + arrayInput.length + ' results</p>\
+    ';
+
+    for (var i=0; i<arrayInput.length; i++) {
+      htmlOutput += '\
+        <div style="width: 635px; padding-left: 190px;">\
+          <div style="width: 100%; padding: 10px; font-size: 14px;">\
+            <a style="font-family: arial, sans-serif; line-height: 1.2em; font-weight: normal; text-decoration: none; font-size: 19px;" href="https://en.wikipedia.org/wiki/' + arrayInput[i].title + '"target="_blank">\
+              <h3 style="line-height: 1.2em;">' + arrayInput[i].title + '</h3>\
+            </a>\
+            <p style="font-family: Arial, sans-serif; color: #008029; line-height: 1.2em; font-size: 15px; ">https://en.wikipedia.org/wiki/' + arrayInput[i].title + '</p>\
+            <p style="line-height: 1.4em;">' + arrayInput[i].snippet + '</p>\
+          </div>\
+        </div>';
     }
 
-    return htmlOutput;ß
+    return htmlOutput;
 
   };
+  
   secondPageHeader = '\
     <div style="background-color: rgb(241, 241, 241); width: 100%; height: 60px; margin: 0; padding: 0;">\
-      <img src="images/Wikipedia-logo-wordmark.png" alt="Wikipedia Logo" style="height: 58px;" />\
+      <a href="."><img src="images/Wikipedia-logo-wordmark.png" alt="Wikipedia Logo" style="height: 58px;" /></a>\
       <input type="text" id="searchBox" style="margin: 0; position: relative; top: -23px;"/>\
       <button id="normalSearch" class="loaded-search" type="submit" style="background-color: rgb( 66, 133, 244); position: relative; top: -23px; left: -5px; height: 41px; width: 41px; font-family: \'Times New Roman\', serif; font-size: 1.25em; color: white; border: none;"><i class="fa fa-search" aria-hidden="true"></i></button>\
     </div>\
@@ -21,10 +37,6 @@ $("document").ready(function() {
       <ul>\
         <li style="width: 30px; font-size: 13px; font-weight: 600; height: 58px; text-align: center; border-bottom: 3px solid rgb(66, 133, 244); color: rgb(66, 133, 244); line-height: 60px; vertical-align: middle;">All</li>\
       </ul>\
-    </div>\
-    ';
-  secondPageArticle = '\
-    <div>\
     </div>\
     ';
 
@@ -60,31 +72,30 @@ $("document").ready(function() {
       $("header").empty();
       $("article").empty();
       // Request list of possible articles.
+      // BAD CODE: ENDLESS MIRROR
       $.ajax({
         url: searchUrl,
         dataType: "jsonp",
         success: function(results) {
           resultsArray = results.query.search;
-          //console.log(results.query.search);
           $("header").html(secondPageHeader);
-          console.log(resultsArray);
-          secondPageArticle = resultsHTML(resultsArray);
-          $("article").html(secondPageArticle); // HTML results go here (referenced by variable)
+          $("article").html( resultsHTML(resultsArray) ); // HTML results go here (referenced by variable)
+          
+          $("#searchBox").attr("value", searchQuery);
+          
+          // Disconnected listener, so have to relisten.
+          $("#searchBox").keypress(function (e) {
+            if(e.which == 13) {searchArticle($("#searchBox").val());}
+          });
+          $("#normalSearch").hover(function() {
+            $(this).css({"background-color": "rgb(59, 120, 231)",});
+          }, function() {
+            $(this).css({"background-color": "rgb(66, 133, 244)"});
+          });
         }
+        
       });
-
-      // Disconnected listener, so have to relisten.
-      $("#searchBox").keypress(function (e) {
-        if(e.which == 13) {searchArticle($("#searchBox").val());}
-      });
-
-      $("#normalSearch").hover(function() {
-        $(this).css({"background-color": "rgb(59, 120, 231)",});
-      }, function() {
-        $(this).css({"background-color": "rgb(66, 133, 244)"});
-      });
-
-      $("#searchBox").attr("value", searchQuery);
+      
     }
 
   //pull title and first paragraph of search object
